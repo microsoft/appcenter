efdf0a5a67d1deee5421e71e9193e434ec9b3938
Richard Norton <github@geekatwork.net>
2019-10-25 11:48:58 +1300

    Fix formatting issue

--- sample-apps/xamarin/acquaint-crm/App/Acquaint.XForms/Acquaint.XForms/Constants/MessageKeys.cs
+++ sample-apps/xamarin/acquaint-crm/App/Acquaint.XForms/Acquaint.XForms/Constants/MessageKeys.cs
@@ -7,13 +7,13 @@
     {
         public const string DisplayAlert = "DisplayAlert";
         public const string DisplayQuestion = "DisplayQuestion";
-		public const string AddAcquaintance = "AddAcquaintance";
+	public const string AddAcquaintance = "AddAcquaintance";
         public const string UpdateAcquaintance = "UpdateAcquaintance";
         public const string DeleteAcquaintance = "DeleteAcquaintance";
         public const string AcquaintanceLocationUpdated = "AcquaintanceLocationUpdated";
         public const string SetupMap = "SetupMap";
-		public const string DataPartitionPhraseValidation = "DataPartitionPhraseValidation";
-		public const string BackendUrlValidation = "BackendUrlValidation";
-	}
+	public const string DataPartitionPhraseValidation = "DataPartitionPhraseValidation";
+	public const string BackendUrlValidation = "BackendUrlValidation";
+    }
 }
 
