0226b54bdae53b04fed7edf03198bbc8875fa1c5
Villars Gimm <villars.gimm@gmail.com>
2019-11-08 16:19:59 +0100

    Add env var requirements

--- sample-build-scripts/test/xamarin-uitest-android/appcenter-post-build.sh
+++ sample-build-scripts/test/xamarin-uitest-android/appcenter-post-build.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+##
+# Environment variables required in App Center Build
+# https://docs.microsoft.com/en-us/appcenter/build/custom/variables/
+#
+# APPCENTER_TOKEN
+# https://docs.microsoft.com/en-us/appcenter/api-docs/
+#
+# DEVICE_SET
+# Both the device slug and a defined device set can be used here
+#
+# APP_OWNER
+# It's a combinations of Organization and app name, i.e., {Organization/App-name}
+#
+# LOCALE
+# UTF-8 locale like en_US for english American
+##
+
+
 echo "Found UI tests projects:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '*.UITest.*\.csproj' -exec echo {} \;
 echo
