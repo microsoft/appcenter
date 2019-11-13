1f48fde73762570582a59fca372d5c55697ad7de
Villars Gimm <villars.gimm@gmail.com>
2019-11-13 16:27:42 +0100

    Test scripts (#1326)
    * Add Xamarin.UITest iOS post-build script
    
    * Add Xamarin.UITest Android post-build script
    
    * Address @maxxkrakoa comments
    
    * Add env var requirements
    
    * Remove redundant find
    
    * Clarifications in comments

---
+++ sample-build-scripts/test/xamarin-uitest-android/appcenter-post-build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+##
+# Environment variables required in App Center Build
+# https://docs.microsoft.com/en-us/appcenter/build/custom/variables/
+#
+# APPCENTER_TOKEN
+# https://docs.microsoft.com/en-us/appcenter/api-docs/
+#
+# DEVICE_SET
+# Both the device slug and a defined device set can be used here
+#
+# APP_OWNER
+# It's a combinations of Organization and app name, i.e., {Organization/App-name}
+#
+# LOCALE
+# UTF-8 locale like en_US for english American
+##
+
+
+if find $APPCENTER_SOURCE_DIRECTORY -name '*.UITest.csproj';
+then
+	echo "Building UI test projects:"
+	find $APPCENTER_SOURCE_DIRECTORY -name '*.UITest.csproj' -exec msbuild {} \;
+else
+	echo "Can't find UI test project"
+	exit 9999
+fi
+echo "Compiled projects to run UI tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITest.*\.dll' -exec echo {} \;
+echo "Running test in App Center Test"
+APPPATH=$APPCENTER_OUTPUT_DIRECTORY/*.apk
+BUILDDIR=$APPCENTER_SOURCE_DIRECTORY/*.UITest/bin/Debug/
+UITESTTOOL=$APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools
+appcenter test run uitest --app $APP_OWNER --devices $DEVICE_SET --test-series "$APPCENTER_BRANCH-$APPCENTER_TRIGGER" --locale $LOCALE --app-path $APPPATH --build-dir $BUILDDIR --async --uitest-tools-dir $UITESTTOOL --token $APPCENTER_TOKEN
