90dc56e05fba2aef123eea0e2d21e70379ec2ddc
Dima <v-dmkira@microsoft.com>
2021-08-06 13:45:28 +0300

    Delete sample-apps/xamarin/acquaint-crm directory
    This application is outdated and it is not demonstrating how App Center works.

--- sample-apps/xamarin/acquaint-crm/App/Acquaint.Native/Acquaint.Native.Droid/Scripts/injectGoogleMapsApiKey.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i '' "s/GOOGLE_MAPS_API_KEY/$2/g" "$1/Properties/AndroidManifest.xml"
--- sample-apps/xamarin/acquaint-crm/App/Common/Acquaint.Abstractions/DataSyncErrorEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Acquaint.Abstractions
-{
-	/// <summary>
-	/// A generically-typed delegate for handling data sync errors.
-	/// </summary>
-	public delegate void DataSyncErrorEventHandler<T>(object sender, DataSyncErrorEventArgs<T> e);
-}
-
